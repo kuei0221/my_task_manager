e2c5a0fce249836a5fce541f622a45746f3dfe29
michaelhwang <michaelhwang0619@gmail.com>
2021-02-21 04:01:57 +0800

    fix: get ride of field_with_error

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,8 @@ module MyTaskManager
     #
     config.time_zone = 'Taipei'
     # config.eager_load_paths << Rails.root.join("extras")
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance|
+      "#{html_tag}".html_safe
+    }
   end
 end
