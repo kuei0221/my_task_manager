1ac0b2b30acdb4598a73e2322574870046790e36
michaelhwang <michaelhwang0619@gmail.com>
2021-02-21 16:01:06 +0800

    model: create user model and add association

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ApplicationRecord
+  belongs_to :user
+
   enum priority: { low: 0, medium: 1, high: 2 }
   validates :name, presence: true
   validates :status, inclusion: { in: %w[pending in_progress completed] }
