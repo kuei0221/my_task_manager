1e9732c6c90be36369a8b298c984471d3a67a888
michaelhwang <michaelhwang0619@gmail.com>
2021-02-21 22:45:45 +0800

    feature(controller): check login status before task action and only can see user's task

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  include ApplicationHelper
+
+  private
+
+  def authenticate_user
+    redirect_to login_path, notice: 'please login first' unless current_user
+  end
 end
