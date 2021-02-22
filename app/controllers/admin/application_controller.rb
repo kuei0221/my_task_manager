5b69b5405d35225fa6f574f122320e00abb25c93
michaelhwang <michaelhwang0619@gmail.com>
2021-02-22 17:51:49 +0800

    feature(controller): only login and admin can enter admin controller

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,5 +1,17 @@
 module Admin
   class ApplicationController < ::ApplicationController
+    before_action :authenticate_user
+    before_action :authenticate_admin_permission
     layout 'admin'
+
+    private
+
+    def authenticate_user
+      redirect_to login_path, notice: 'please login first' unless current_user
+    end
+
+    def authenticate_admin_permission
+      redirect_to tasks_path, notice: 'You do not have the permission to enter this page' unless current_user.admin?
+    end
   end
 end
