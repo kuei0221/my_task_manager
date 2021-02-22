995aa0257cca24b53e7c8edec979a25cff621b3d
michaelhwang <michaelhwang0619@gmail.com>
2021-02-22 17:52:14 +0800

    feature(model): prevent from delete last admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,17 @@
 class User < ApplicationRecord
   has_many :tasks, dependent: :delete_all
   has_secure_password
+
+  validates :name, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+
+  before_destroy :last_admin_user?, if: :admin?
+
+  private
+
+  def last_admin_user?
+    raise LastAdmin, I18n.t('user.last_admin_error') if User.where(admin: true).size == 1
+  end
+
+  class LastAdmin < StandardError; end
 end
