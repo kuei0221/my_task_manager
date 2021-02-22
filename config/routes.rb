731f6e98e420201942b68b34b90754066487460c
michaelhwang <michaelhwang0619@gmail.com>
2021-02-22 23:29:33 +0800

    config: add task_labels routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'tasks#index'
-  resources :tasks
+  resources :tasks do
+    member do
+      resources :task_labels, only: %i[new create destroy]
+    end
+  end
   get '/login', to: 'sessions#new', as: :login
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy', as: :logout
