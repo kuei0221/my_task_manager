0c7d4e0c221e83616911ea9e17866735594b9baf
michaelhwang <michaelhwang0619@gmail.com>
2021-02-23 03:28:57 +0800

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create(id: 1, name: 'michael')
+User.create(id: 1, name: 'michael', password: 'password', admin: true)
